833af9db9135dbc38cac5fd8b9434263fb827eda
VidushAgarwal <59796609+VidushAgarwal@users.noreply.github.com>
2022-07-24 22:22:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('odd or even')
 a = st.number_input('Enter a number')
-if (x).is_integer():
+if (a).is_integer():
     if a%2==0:
         result = "even"
     else:
